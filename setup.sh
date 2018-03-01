0e7b2546d2e67fc5cd26f1ac38eb862e12e9c5b5
Stephen Wardill <s5n.wardill@gmail.com>
2018-03-01 13:31:16 +0000

    create a link for ~/.bashrc to the repo

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ mkdir ~/.vim/{swapfiles,backupdir,undodir}
 ln -s $GITHUBLOCAL/paleo9/configs/i3/config ~/.configs/i3/config
 ln -s $GITHUBLOCAL/paleo9/configs/vim/vimrc ~/.vimrc
 ln -s $GITHUBLOCAL/paleo9/configs/UltiSnips ~/.vim/UltiSnips
+ln -s $GITHUBLOCAL/paleo9/configs/bash/bashrc ~/.bashrc
